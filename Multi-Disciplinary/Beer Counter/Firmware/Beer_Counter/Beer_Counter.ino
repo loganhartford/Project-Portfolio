301977a49865db379512210a201c1aeaf21788a6
loganhartford <56402946+loganhartford@users.noreply.github.com>
2022-07-01 14:12:13 -0400

    Majority of beer counter code is done

--- Multi-Disciplinary/Beer Counter/Firmware/Beer_Counter/Beer_Counter.ino
+++ Multi-Disciplinary/Beer Counter/Firmware/Beer_Counter/Beer_Counter.ino
@@ -11,15 +11,25 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 // Pins
 #define BUTTON 2  // User input mom switch
 #define MOTION 3  // Motion sensor input
+#define BUZZ 4    // Buzzer
+#define RED 5     // RED diode of RGB LED
+#define GREEN 6   // GREEN diode of RGB LED
+#define BLUE 9    // BLUE diode of RGB LED
 #define LED 13    // On board or external LED
 
 #define PHOTO A0  // PHOTO Resisotr Input
 #define POT_1 A1  // Potentiometer 1 Input
 
 // Constants
+// EEPROM memory locations
+#define ONES 0
+#define TENS 1
+#define HUNDREDS 2
+#define THOUSANDS 3
+#define UPDATE_REQ 9
 
 // Variables
-uint16_t beers = 0;
+int16_t beers = 0;
 volatile bool motion_detected = 0;
 
 
@@ -32,6 +42,8 @@ void init_screen(void);         // Reads in initial EEPROM values and displays t
 uint16_t update_count(void);    // Reads in EEPROM values, increments and the stores them
 void update_screen(int beers);  // Updates screen with current number of beers
 void analog_reads(void);        // Reads all analog inputs
+void RGB_LED(uint8_t red, uint8_t green, uint8_t blue);
+void meme_detector(int16_t beers);
 void button_ISR(void);          // Services momentary switch
 void motion_ISR(void);          // Services motion detector
 
@@ -53,12 +65,27 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(MOTION), motion_ISR, RISING);
 
   // Pin Set Up
-  pinMode(LED, OUTPUT); // On board or external LED
-
+  pinMode(BUZZ, OUTPUT);  // Buzzer
+  pinMode(RED, OUTPUT);   // RED pin of REG LED
+  pinMode(GREEN, OUTPUT); // GREEN pin of REG LED
+  pinMode(BLUE, OUTPUT);  // BLUE pin of REG LED
+  pinMode(LED, OUTPUT);   // On board or external LED
+  
+  digitalWrite(BUZZ, LOW);
+  digitalWrite(LED, LOW);
+  analogWrite(RED, 0);
+  analogWrite(GREEN, 0);
+  analogWrite(BLUE, 0);
 
   // Initialize the screen for this application
   init_screen();
-  
+
+  // Checks to see if the code needs to be updated before it will work
+  if (EEPROM.read(UPDATE_REQ) == 111)
+  {
+    // This will throw the code into an infinite loop
+    meme_detector(9999);
+  }
 }
 
 void loop() 
@@ -69,9 +96,9 @@ void loop()
       update_screen(beers);
       motion_detected = 0;
   }
-
   analog_reads();
 }
+
 
 /*
  *        Functions
@@ -85,29 +112,26 @@ void loop()
  */
 void init_screen(void)
 {
-  uint8_t data[4];
-  for (int i = 0; i<4; i++)
+  // Read in data from EEPROM
+  uint8_t ones = EEPROM.read(ONES);
+  uint8_t tens = EEPROM.read(TENS);
+  uint8_t hundreds = EEPROM.read(HUNDREDS);
+  uint8_t thousands = EEPROM.read(THOUSANDS);
+
+  // Check to see if the addresses have been written to before, 255 is default.
+  if ((ones == 255) || (tens == 255) || (hundreds == 255) || (thousands == 255))
   {
-    // Read in the counter data from EEPROM
-    data[i] = EEPROM.read(i);
-    // Check to see if the address has been written to before
-    if (data[i] == 255)   // 255 is default value
-    {
-      // if it has not been written to before, set to 0
-      data[i] = 0;
-      EEPROM.write(i, data[i]);
-    }
+    // Store new value in EEPROM
+    EEPROM.write(ONES, 0);
+    EEPROM.write(TENS, 0);
+    EEPROM.write(HUNDREDS, 0);
+    EEPROM.write(THOUSANDS, 0);
   }
   // Create a single int from data
-  uint8_t ones = data[0];
-  uint8_t tens = data[1];
-  uint8_t hundreds = data[2];
-  uint8_t thousands = data[3];
   beers = ones + (10*tens) + (100*hundreds) + (1000*thousands);
   
   // Update the screen with the initial counter
   update_screen(beers);
-  
 }
 
 /*
@@ -120,10 +144,10 @@ uint16_t update_count(void)
 {
   uint16_t temp;
   // Read in values from EEPROM
-  uint8_t ones = EEPROM.read(0);
-  uint8_t tens = EEPROM.read(1);
-  uint8_t hundreds = EEPROM.read(2);
-  uint8_t thousands = EEPROM.read(3);
+  uint8_t ones = EEPROM.read(ONES);
+  uint8_t tens = EEPROM.read(TENS);
+  uint8_t hundreds = EEPROM.read(HUNDREDS);
+  uint8_t thousands = EEPROM.read(THOUSANDS);
 
   // Turn into a single integer
   temp = ones + (10*tens) + (100*hundreds) + (1000*thousands);
@@ -135,18 +159,36 @@ uint16_t update_count(void)
   thousands = (temp - hundreds - tens - ones)/1000;
 
   // Store new value in EEPROM
-  EEPROM.write(0, ones);
-  EEPROM.write(1, tens);
-  EEPROM.write(2, hundreds);
-  EEPROM.write(3, thousands);
-  return temp;
+  EEPROM.write(ONES, ones);
+  EEPROM.write(TENS, tens);
+  EEPROM.write(HUNDREDS, hundreds);
+  EEPROM.write(THOUSANDS, thousands);
+
+  // Check to make sure values were written into EEPROM correctly
+  if((EEPROM.read(ONES) == ones) || (EEPROM.read(TENS) == tens) || 
+  (EEPROM.read(HUNDREDS) == hundreds) || (EEPROM.read(THOUSANDS) == thousands))
+  {
+    // Beep the buzzer twice for succees
+    digitalWrite(BUZZ, HIGH);
+    delay(75);
+    digitalWrite(BUZZ, LOW);
+    return temp;
+  }
+  else 
+  {
+    // One long beep for failure
+    digitalWrite(BUZZ, HIGH);
+    delay(500);
+    digitalWrite(BUZZ, LOW);
+    return -1;
+  }
 }
 
 /*
  * Updates the LCD screen with the current number of beers
  * and they're equivalent scrap value.
  */
-void update_screen(int beers)
+void update_screen(int16_t beers)
 {
   uint8_t space = 0;
   if (beers < 10)
@@ -196,6 +238,73 @@ void analog_reads(void)
   else
   {
     lcd.backlight();
+  }
+}
+
+/*
+ * A quick way to change the state of the RGB LED
+ */
+void RGB_LED(uint8_t red, uint8_t green, uint8_t blue)
+{
+  analogWrite(RED, red);
+  analogWrite(GREEN, green);
+  analogWrite(BLUE, blue);
+}
+
+/*
+ * Detects when the number of beers is some special or funny
+ * number and does a special animation with lights, screen
+ * and LED.
+ */
+void meme_detector(int16_t beers)
+{
+  // 6
+  if (beers == 6)
+  {
+    
+  }
+  // 69
+  else if (beers == 69)
+  {
+    
+  }
+  // 420
+  else if (beers == 420)
+  {
+    
+  }
+  // 666
+  else if (beers == 666)
+  {
+    
+  }
+  // 777
+  else if (beers == 777)
+  {
+    
+  }
+  // 1111
+  else if (beers == 1111)
+  {
+    
+  }
+  // 6969
+  else if (beers == 6969)
+  {
+    
+  }
+  // 9999
+  else if (beers == 9999)
+  {
+    // Code is only designed to handle upto 9999 beers
+    RGB_LED(255, 0, 0); // Set LED to red
+    lcd.setCursor(0,0);
+    lcd.print("Update Required");
+    lcd.setCursor(0,1);
+    lcd.print("Talk to Motto");
+    // Wrtie EEPROM update REQ can be detected on start-up
+    EEPROM.write(UPDATE_REQ, 111);
+    while (1);
   }
 }
 
